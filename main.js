27a1e31bb4ea0ee8a4a5386d452926f186d22fc8
lhama605 <queiroz.alana@escola.pr.gov.br>
2026-05-13 15:16:25 +0000

    aula 3 parte 1

--- main.js
+++ main.js
@@ -12,3 +12,14 @@ for (let i = 0; i < botoes.length; i++) {
     abas[i].classList.add("ativo");
   };
 }
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  };
+}
